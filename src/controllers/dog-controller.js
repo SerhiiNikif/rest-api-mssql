81bb17d141e8e5b9681128882ec34f6d7edddcbc
Serhii <nikiforenko.sergiy@gmail.com>
2023-06-17 15:29:25 +0300

    Query for the findAll function

--- src/controllers/dog-controller.js
+++ src/controllers/dog-controller.js
@@ -2,7 +2,32 @@ const db = require("../models");
 const Dog = db.dogs;
 
 const findAll = async (req, res) => {
-    Dog.findAll() 
+    let {attribute, order, pageNumber, limit} = req.query;
+    const paramQuerySQL = {};
+
+    if (typeof order === 'undefined' || order == '') {
+        order = 'ASC';
+    }
+
+    if (typeof order !== 'undefined' && !['asc','desc'].includes(order.toLowerCase())) {
+        order = 'ASC';
+    }
+
+    if (attribute != '' && typeof attribute !== 'undefined') {
+        paramQuerySQL.order = [
+            [attribute, order]
+        ];
+    }
+
+    if (pageNumber != '' && typeof pageNumber !== 'undefined' && pageNumber > 0) {
+        paramQuerySQL.offset = parseInt(pageNumber);
+    }
+
+    if (limit != '' && typeof limit !== 'undefined' && limit > 0) {
+        paramQuerySQL.limit = parseInt(limit);
+    }
+
+    Dog.findAll(paramQuerySQL)
         .then(data => {
             res.status(200).json(data);
         })
